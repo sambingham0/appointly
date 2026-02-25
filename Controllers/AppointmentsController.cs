2098acb7010f584418e8d5faacab97ba5f82a7a7
vbisso <valeriabisso19@gmail.com>
2026-02-24 20:00:54 -0700

    files created

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,6 @@
+//standar crud for Appointments
+//Index() - list appointments (usually include Team and CreatedByUser)
+//Details(id) - show appointment info + created by user. team?, list of participants?, UI for adding/removing participants
+//Create() - create new appointment
+//Edit(id) - edit existing appointment
+//Delete(id) - delete appointment
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,6 @@
+//standar crud for Users. 
+//views: index() -> lists all users
+//       details(id) -> details for one user. nice to have: teams the belong to?, appointments participated in?, appointments user crated?
+//       create() -> create new user
+//       edit(id) -> edit existing user
+//       delete(id) -> delete user
